5dd78758332aeaf0fb9c9df3c5ff6dac49957008
Haomin Wang <898496136@qq.com>
2025-11-10 20:44:00 +0800

    修正email为空的问题

--- FIreRescueManageSystem/FireRescueManageSystem/src/main/java/com/skypapaya/firerescuemanagesystem/DO/UserDO.java
+++ FIreRescueManageSystem/FireRescueManageSystem/src/main/java/com/skypapaya/firerescuemanagesystem/DO/UserDO.java
@@ -1,6 +1,7 @@
 package com.skypapaya.firerescuemanagesystem.DO;
 
 import lombok.Data;
+import org.apache.catalina.User;
 
 import java.time.LocalDateTime;
 
@@ -17,5 +18,21 @@ public class UserDO {
     String password;
     LocalDateTime gmtCreated;
     LocalDateTime gmtModified;
+
+    public UserDO() {}
+
+    public UserDO(String name, String eMail, String phone, String sex, String password, int age, String address) {
+        this.name = name;
+        this.eMail = eMail;
+        this.phone = phone;
+        this.sex = sex;
+        this.password = password;
+        this.age = age;
+        this.address = address;
+    }
+
+    public String toString() {
+        return "UserDO{" +"id=" + id + ", name='" + name + '\'' + ", age=" + age + ", eMail='" + eMail + '\'' + ", phone='" + phone + '\'' + ", sex='" + sex + '\'' + ", authority='" + authority + '\'' + ", address='" + address + '\'' + ", password='" + password + '\'' + "}";
+    }
 
 }
